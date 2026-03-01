d3d80d5fbee9f5773463a471d350042f898f5347
DEALMRKT <rahom3025@gmail.com>
2026-03-01 12:57:28 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'DEALMRKT | Маркетплейс',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  )
+    }
